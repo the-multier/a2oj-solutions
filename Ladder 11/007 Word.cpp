09241672850f433c77bba6afaf9e3c8756e97899
Abhinav Arun <abhinavarun123@gmail.com>
2021-10-17 03:37:11 +0530

    Add File

---
+++ Ladder 11/007 Word.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int upper = 0, lower = 0;
+    string word;
+
+    cin >> word;
+    int l = word.length();
+
+    for (int i = 0; i < l; i++)
+    {
+        if (word[i] >= 65 && word[i] <= 90)
+        {
+            upper += 1;
+        }
+        else if (word[i] >= 97 && word[i] <= 122)
+        {
+            lower += 1;
+        }
+    }
+    if (upper > lower)
+    {
+        for (int i = 0; i <= l; i++)
+        {
+            if (word[i] >= 97 && word[i] <= 122)
+                word[i] = word[i] - 32;
+        }
+    }
+    else
+    {
+        for (int i = 0; i <= l; i++)
+        {
+            if (word[i] >= 65 && word[i] <= 90)
+                word[i] = word[i] + 32;
+        }
+    }
+
+    cout << word;
+
+    return 0;
+}
