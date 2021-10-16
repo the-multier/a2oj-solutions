05ae6895a3889fed612be9caf19b1695fdcfab12
Abhinand Arun <abhinandarun02com@gmail>
2021-10-17 00:14:10 +0530

    Added File

---
+++ Ladder 11/002 Beautiful Matrix.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x, y, ans, nums[6][6];
+
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 1; j < 6; j++)
+        {
+            cin >> nums[i][j];
+            if (nums[i][j] == 1)
+            {
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    cout << abs(x - 3) + abs(y - 3);
+
+    return 0;
+}
