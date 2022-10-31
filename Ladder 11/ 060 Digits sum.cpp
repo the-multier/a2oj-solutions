40754f7de336552723c3a455eb49e1301296f235
Prabathkini <92639792+Prabathkini@users.noreply.github.com>
2022-10-31 19:20:37 +0530

    digits sum done

---
+++ Ladder 11/ 060 Digits sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+       int t;
+       cin>>t;
+       while(t--){
+           long n;
+           cin>>n;
+           if(n%10==9)
+              cout<<n/10+1<<endl;
+           else
+              cout<<n/10<<endl;
+       }
+       return 0;
+}
